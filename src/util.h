2332651f1342b2e8b933ba93f0b756c4734e298b
Fabian Schlieper <fabian@schlieper.email>
2018-03-03 09:25:32 +0100

    fix to8601

--- src/util.h
+++ src/util.h
@@ -8,37 +8,37 @@
 #include "../date/include/date/date.h"
 
 namespace influxdb {
-    namespace util {
-        using namespace date;
-        using namespace std;
+	namespace util {
+		using namespace date;
+		using namespace std;
 
-        static std::string formatByte(unsigned char value) {
-            std::stringstream ss;
-            ss << std::setfill('0') << std::setw(2) << std::hex << std::uppercase
-               << static_cast<unsigned int>(value);
-            return ss.str();
-        }
+		static std::string formatByte(unsigned char value) {
+			std::stringstream ss;
+			ss << std::setfill('0') << std::setw(2) << std::hex << std::uppercase
+			   << static_cast<unsigned int>(value);
+			return ss.str();
+		}
 
-        static std::string urlEncode(const std::string &stri) {
-            std::string result;
-            result.reserve(stri.length() + 1);
-            auto str = stri.c_str();
-            for (char ch = *str; ch != '\0'; ch = *++str) {
-                switch (ch) {
-                    case '%':
-                    case '=':
-                    case '&':
-                    case '\n':
-                    case ' ':
-                        result.append("%" + formatByte(static_cast<unsigned char>(ch)));
-                        break;
-                    default:
-                        result.push_back(ch);
-                        break;
-                }
-            }
-            return result;
-        }
+		static std::string urlEncode(const std::string &stri) {
+			std::string result;
+			result.reserve(stri.length() + 1);
+			auto str = stri.c_str();
+			for (char ch = *str; ch != '\0'; ch = *++str) {
+				switch (ch) {
+					case '%':
+					case '=':
+					case '&':
+					case '\n':
+					case ' ':
+						result.append("%" + formatByte(static_cast<unsigned char>(ch)));
+						break;
+					default:
+						result.push_back(ch);
+						break;
+				}
+			}
+			return result;
+		}
 
 /*
         std::string timeToStr(long epoch) {
@@ -68,87 +68,101 @@ namespace influxdb {
         */
 
 
-        static date::sys_time<std::chrono::milliseconds>
-        parse8601(std::istream &&is) {
-            std::string save;
-            is >> save;
+		static date::sys_time<std::chrono::milliseconds>
+		parse8601(std::istream &&is) {
+			std::string save;
+			is >> save;
 
-            try {
+			try {
 
-                std::istringstream in{save};
-                date::sys_time<std::chrono::milliseconds> tp;
-                in >> date::parse("%FT%TZ", tp);
-                if (in.fail()) {
-                    in.clear();
-                    in.exceptions(std::ios::failbit);
-                    in.str(save);
-                    in >> date::parse("%FT%T%Ez", tp);
-                }
-                return tp;
-            } catch (const std::exception &e) {
-                throw std::runtime_error("parse8601('" + save + "') failed");
-            }
-        }
+				std::istringstream in{save};
+				date::sys_time<std::chrono::milliseconds> tp;
+				in >> date::parse("%FT%TZ", tp);
+				if (in.fail()) {
+					in.clear();
+					in.exceptions(std::ios::failbit);
+					in.str(save);
+					in >> date::parse("%FT%T%Ez", tp);
+				}
+				return tp;
+			} catch (const std::exception &e) {
+				throw std::runtime_error("parse8601('" + save + "') failed");
+			}
+		}
 
-        static date::sys_time<std::chrono::milliseconds>
-        parseHttpDate(std::istream &&is) {
-            std::string save;
-            is >> save;
-            try {
-                static std::locale enUS("en_US");
-                std::istringstream in{save};
-                in.imbue(enUS);
-                date::sys_time<std::chrono::milliseconds> tp;
-                // Tue, 15 Nov 1994 12:45:26 GMT
-                in >> date::parse("%a, %d %b %Y %T %Z", tp); // "D, d M Y H:i:s T%FT%TZ"
-                return tp;
-            } catch (const std::exception &e) {
-                throw std::runtime_error("parseHttpDate('" + save + "') failed");
-            }
-        }
+		static date::sys_time<std::chrono::milliseconds>
+		parseHttpDate(std::istream &&is) {
+			std::string save;
+			is >> save;
+			try {
+				static std::locale enUS("en_US");
+				std::istringstream in{save};
+				in.imbue(enUS);
+				date::sys_time<std::chrono::milliseconds> tp;
+				// Tue, 15 Nov 1994 12:45:26 GMT
+				in >> date::parse("%a, %d %b %Y %T %Z", tp); // "D, d M Y H:i:s T%FT%TZ"
+				return tp;
+			} catch (const std::exception &e) {
+				throw std::runtime_error("parseHttpDate('" + save + "') failed");
+			}
+		}
 
-        inline date::sys_time<std::chrono::milliseconds>
-        parse8601(const std::string &str) { return parse8601(istringstream{str}); }
+		inline date::sys_time<std::chrono::milliseconds>
+		parse8601(const std::string &str) { return parse8601(istringstream{str}); }
 
-        inline date::sys_time<std::chrono::milliseconds>
-        parseHttpDate(const std::string &str) { return parseHttpDate(istringstream{str}); }
+		inline date::sys_time<std::chrono::milliseconds>
+		parseHttpDate(const std::string &str) { return parseHttpDate(istringstream{str}); }
 
-        inline std::string to8601(const date::sys_time<std::chrono::milliseconds> &tp) {
-            return date::format("%FT%TZ", tp);
-        }
+		//   inline std::string to8601(const date::sys_time<std::chrono::milliseconds> &tp) {
+		//      return date::format("%FT%TZ", tp);
+		//   }
 
-        inline std::string to8601(int64_t epochMs) {
-            const static date::sys_time<std::chrono::milliseconds> tp_epoch;
-            return to8601(tp_epoch + std::chrono::milliseconds(epochMs));
-        }
+		inline std::string to8601(int64_t epochMs) {
+			const static date::sys_time<std::chrono::milliseconds> tp_epoch;
+			return date::format("%FT%TZ", tp_epoch + std::chrono::milliseconds(epochMs));
+		}
 
-        inline std::string to8601(const std::chrono::time_point<std::chrono::milliseconds> &tp) {
-            return to8601(tp.time_since_epoch().count());
-        }
+		// inline std::string to8601(const std::chrono::time_point<std::chrono::milliseconds> &tp) {
+		//      return to8601(tp.time_since_epoch().count());
+		// }
 
 
-        inline std::string to8601(const std::chrono::system_clock::time_point &tp) {
-            return to8601(std::chrono::duration_cast<std::chrono::milliseconds>(tp.time_since_epoch()).count());
-        }
+		//inline std::string to8601(const std::chrono::system_clock::time_point &tp) {
+		//	return to8601(std::chrono::duration_cast<std::chrono::milliseconds>(tp.time_since_epoch()).count());
+		//}
 
-        static bool replace(std::string &str, const std::string &from, const std::string &to) {
-            size_t start_pos = str.find(from);
-            if (start_pos == std::string::npos)
-                return false;
-            str.replace(start_pos, from.length(), to);
-            return true;
-        }
-    }
+		template<class T, typename = std::enable_if_t<!std::is_integral<T>::value>>
+		inline std::string to8601(const T &tp) {
+			return to8601(std::chrono::duration_cast<std::chrono::milliseconds>(tp.time_since_epoch()).count());
+		}
 
-    inline int wsaStart() {
+		template<class T, typename = std::enable_if_t<!std::is_integral<T>::value>>
+		inline std::string to8601(const T &&tp) {
+			return to8601(std::chrono::duration_cast<std::chrono::milliseconds>(tp.time_since_epoch()).count());
+		}
+
+		//template<>
+		//inline std::string to8601<int64_t>(const int64_t &tp) { return to8601_(tp); }
+
+
+		static bool replace(std::string &str, const std::string &from, const std::string &to) {
+			size_t start_pos = str.find(from);
+			if (start_pos == std::string::npos)
+				return false;
+			str.replace(start_pos, from.length(), to);
+			return true;
+		}
+	}
+
+	inline int wsaStart() {
 #if defined(_WIN32) && defined(MAKEWORD)
-        WORD wVersionRequested;
-        WSADATA wsaData;
-        wVersionRequested = MAKEWORD(2, 2);
-        WSAStartup(wVersionRequested, &wsaData);
+		WORD wVersionRequested;
+		WSADATA wsaData;
+		wVersionRequested = MAKEWORD(2, 2);
+		WSAStartup(wVersionRequested, &wsaData);
 #endif
-        return 0;
-    }
+		return 0;
+	}
 
 
 }
